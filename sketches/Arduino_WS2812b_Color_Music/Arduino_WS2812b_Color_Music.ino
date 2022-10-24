0542f97c0f36c016e42747a4f0b995fa464b51c6
Igor Peshkov <igor.peshkov@gmail.com>
2022-10-24 22:26:12 +0300

    Button fixes. Add IR library (don't work correctly)

--- sketches/Arduino_WS2812b_Color_Music/Arduino_WS2812b_Color_Music.ino
+++ sketches/Arduino_WS2812b_Color_Music/Arduino_WS2812b_Color_Music.ino
@@ -1,20 +1,29 @@
 #include "FastLED.h"
+#include "IRremote.h"
 
-#define BTN_MODE_SWT_PIN 2 // Button for change color mode
+#define BTN_MODE_SWT_PIN 2 // Button PIN for change color mode
+#define IR_RECEIVER_MODE_SWT_PIN 3 // IR PIN for control modes by remote control
 
 #define MODE_COUNT 2 // count of color modes
 
-#define SPEED_0 250
+#define SPEED_0 150
 #define SPEED_1 10
 
-#define NUM_LEDS 150 // 290. Count of leds
+#define LED_TYPE WS2811
+#define NUM_LEDS 50 // 290. Count of leds
 #define LED_DT 12 // Pin to cotrol ws812b leds 
 
-boolean btn_mode_swt_last_state = LOW;
+// IR vars
+//IRrecv irrecv(IR_MODE_SWT_PIN);
+decode_results results;
+
+// Switch mode button vars
+boolean btn_mode_swt_last_state = LOW; // last state for switch mode button
 boolean btn_mode_swt_current_state = LOW;
 
 byte current_mode = 0; // Currect color mode
 
+// WS2812B vars
 int max_bright = 30;
 int start_from = 0;
 
@@ -115,27 +124,78 @@ void setup() {
 
   pinMode(BTN_MODE_SWT_PIN, INPUT_PULLUP); // pin mode for switchin modes button with pull up resistor
 
-  LEDS.setBrightness(max_bright);  // ограничить максимальную яркость
+  FastLED.setBrightness(max_bright);  // ограничить максимальную яркость
 
-  FastLED.addLeds<WS2811, LED_DT, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, LED_DT, GRB>(leds, NUM_LEDS);
 
-  //(0, 0, 0);          // погасить все светодиоды
-  LEDS.show();
+  FastLED.clear();
+  FastLED.show();
+
+  IrReceiver.begin(IR_RECEIVER_MODE_SWT_PIN, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN); // Enable IR for switching modes
 }
 
 
 void loop() {
+  //Serial.print("Current mode: ");
+  //Serial.println(current_mode);
+  //delay(1000);
+
+  //The function decode(&results)) is deprecated and may not work as expected! Just use decode() without a parameter and IrReceiver.decodedIRData.<fieldname> .
+
+  if (IrReceiver.decode()) // если данные пришли выполняем команды
+   {
+     Serial.print("IR code: ");
+     Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+
+    //  IrReceiver.printIRResultShort(&Serial);
+
+    //  if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+    //         // We have an unknown protocol here, print more info
+    //         IrReceiver.printIRResultRawFormatted(&Serial, true);
+    //     }
+    // Serial.println();
+
+      /*
+      16761405 - button play
+      */
+
+     switch (IrReceiver.decodedIRData.decodedRawData) {
+       case 0xF807FF00: // button "-"
+        if (--current_mode < 0) current_mode = MODE_COUNT - 1;
+        break;
+       case 0xEA15FF00: // button "+"
+        if (++current_mode >= MODE_COUNT) current_mode = 0;
+        break;
+      case 0xF30CFF00: // button "1"
+        current_mode = 0;
+        break;
+      case 0xE718FF00: // button "2"
+        current_mode = 1;
+        break;
+      break;        
+     }
+
+     IrReceiver.resume(); 
+   }
+
+  // check if button was pressed
   btn_mode_swt_current_state = debounce(BTN_MODE_SWT_PIN, btn_mode_swt_last_state);
 
-  if (btn_mode_swt_last_state == LOW && btn_mode_swt_current_state == HIGH)
+  Serial.print("last: ");
+     Serial.println(btn_mode_swt_last_state);
+     
+  Serial.print("current: ");
+     Serial.println(btn_mode_swt_current_state);
+
+  // if last state was low and curret low change mode. Current state low as pressed because used INPUT_PULLUP pin mode
+  if (btn_mode_swt_last_state == HIGH && btn_mode_swt_current_state == LOW)
   {
     if (++current_mode >= MODE_COUNT) current_mode = 0;
   }
 
   btn_mode_swt_last_state = btn_mode_swt_current_state;
 
-  Serial.println(current_mode);
-
+  // check current mode and run function for show leds
   switch (current_mode) {
     case 0:
       mode_0();
@@ -147,5 +207,5 @@ void loop() {
       break;
   }
 
-  LEDS.show();
+  FastLED.show();    
 }
