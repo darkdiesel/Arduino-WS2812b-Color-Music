2becb5617767254d87cc70d39eb2d99196fce838
Igor Peshkov <igor.peshkov@gmail.com>
2022-10-26 06:09:27 +0300

    Add second button to switch device mode. Add 2 indicator leds to show current device mode. Update code

--- sketches/Arduino_WS2812b_Color_Music/Arduino_WS2812b_Color_Music.ino
+++ sketches/Arduino_WS2812b_Color_Music/Arduino_WS2812b_Color_Music.ino
@@ -2,18 +2,21 @@
 #include "FastLED.h"
 
 #define LED_TYPE WS2811 // led type
-#define NUM_LEDS 150     // 290. Count of leds
+#define NUM_LEDS 100     // 290. Count of leds
 #define LED_PIN 12       // Pin to cotrol ws812b leds 
 
 #define LED_BRIGHTNESS_STEP 10 // step for change led brightness
 
 int cur_brightness = 30;
+
 int start_from = 0;
 
+// mode 2 vars
 #define MODE_2_LENGTH_COLOR_SEGMENT 5
 int mode_2_length_rainbow = 0;
 int mode_2_step = 0;
 
+// mode 3 vars
 int mode_3_ball_position = 0;
 int mode_3_ball_width = 5;
 int mode_3_ball_direction = 1;
@@ -26,7 +29,7 @@ int colors[][3] =
 {
   {255, 0, 0}, // red
   {255, 128, 0}, // orange
- {255, 255, 0}, // yellow 
+  {255, 255, 0}, // yellow 
   {0, 255, 0}, // green
   {0, 255, 255}, // cyan
   {0, 0, 255}, // blue
@@ -36,12 +39,18 @@ int colors[][3] =
 CRGB leds[NUM_LEDS];
 
 // led modes
-#define MODE_COUNT 4 // count of color modes
+// #define MODE_COUNT 4 // count of color modes
 
-#define MODE_SPEED_0 150
-#define MODE_SPEED_1 10
-#define MODE_SPEED_2 35
-#define MODE_SPEED_3 100
+// @TODO: rewrite to count modes from this array and try to define it
+const int COLOR_MODE_SPEED [4] =
+{
+  150,
+  10,
+  35,
+  150
+};
+
+const byte MODE_COUNT ((sizeof(COLOR_MODE_SPEED) / sizeof(COLOR_MODE_SPEED[0])));
 
 unsigned long mode_timer; // timer for print mode
 unsigned long global_timer; // global timer
@@ -74,16 +83,27 @@ volatile boolean ir_flag;
 boolean device_state = true; // on/off state for device
 #define GLOBAL_LOOP 2 // global timer loop
 
-// Switch mode button vars
-#define BTN_MODE_SWT_PIN 2 // Button PIN for change color mode
+// Switch color mode button vars
+#define BTN_COLOR_MODE_SWT_PIN 2 // Button PIN for change color mode
 
-boolean btn_mode_swt_last_state = LOW; // last state for switch mode button
-boolean btn_mode_swt_current_state = LOW;
+boolean btn_color_mode_swt_last_state = LOW; // last state for switch color mode button
+boolean btn_color_mode_swt_current_state = LOW;
 
-byte current_mode = 0; // Current color mode
+byte current_color_mode = 0; // Current mode for color mode
+byte current_music_mode = 0; // Current mode for music mode
 
-boolean debounce(uint8_t btn_pin, boolean last)
-{
+// Swith device mode (color or music)
+#define BTN_DEVICE_MODE_SWT_PIN 4 // Button PIN for change device mode
+byte current_device_mode = 0; // Current mode for device (0: color / 1: music)
+
+boolean btn_device_mode_swt_last_state = LOW; // last state for switch color mode button
+boolean btn_device_mode_swt_current_state = LOW;
+
+// Device mode indicator leds 
+#define LED_DEVICE_MODE_COLOR_PIN 6
+#define LED_DEVICE_MODE_MUSIC_PIN 5
+
+boolean debounce(uint8_t btn_pin, boolean last) {
   boolean current = digitalRead(btn_pin);
 
   if (last != current)
@@ -102,7 +122,7 @@ void one_color_all(int cred, int cgrn, int cblu) {       //-SET ALL LEDS TO ONE 
 }
 
 // moved 7 colors of rainbow to 1 led every tik
-void mode_0() {
+void mode_color_0() {
   int colors_count = (sizeof(colors) / sizeof(colors[0]));
   int color_num = start_from;
 
@@ -121,7 +141,7 @@ void mode_0() {
   }
 }
 
-void mode_1() {
+void mode_color_1() {
   if (colors[start_from][0] == r && colors[start_from][1] == g && colors[start_from][2] == b) {
     start_from++;
 
@@ -154,7 +174,7 @@ void mode_1() {
 }
 
 // big rainbow
-void mode_2() {
+void mode_color_2() {
   int colors_count = (sizeof(colors) / sizeof(colors[0]));
   int color_num = start_from;
 
@@ -178,7 +198,7 @@ void mode_2() {
 }
 
 // ball
-void mode_3() {
+void mode_color_3() {
   for (int i = 0 ; i <= NUM_LEDS; i++ ) {
     if ((i >= mode_3_ball_position) && (i <= (mode_3_ball_position + mode_3_ball_width - 1))) {
       leds[i].setRGB( colors[4][0], colors[4][1], colors[4][2]);
@@ -200,20 +220,46 @@ void mode_3() {
 void setup() {
   Serial.begin(9600);
 
-  pinMode(BTN_MODE_SWT_PIN, INPUT_PULLUP); // pin mode for switchin modes button with pull up resistor
+  // setup buttons for switching device and color modes
+  pinMode(BTN_COLOR_MODE_SWT_PIN, INPUT_PULLUP); // pin mode for switchin color modes button with pull up resistor
+  pinMode(BTN_DEVICE_MODE_SWT_PIN, INPUT_PULLUP); // pin mode for switchin device modes button with pull up resistor
 
+  // setup indicator leds for device mode
+  pinMode(LED_DEVICE_MODE_COLOR_PIN, OUTPUT); // led for color mode
+  pinMode(LED_DEVICE_MODE_MUSIC_PIN, OUTPUT); // led for music mode
+
+  // run device mode leds
+  change_device_mode();
+
+  // setup led
   FastLED.setBrightness(cur_brightness);  // set leds brightness
-
   FastLED.addLeds<LED_TYPE, LED_PIN, GRB>(leds, NUM_LEDS);
 
+  // make lead black before start
   FastLED.clear();
   FastLED.show();
 
   //IrReceiver.begin(IR_RECEIVER_MODE_SWT_PIN, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN); // Enable IR for switching modes
 
+  // setup IR for remote control
   IRLremote.begin(IR_RECEIVER_MODE_SWT_PIN);
+
+  Serial.print("Count of mode: ");  
+  Serial.println(MODE_COUNT);
 }
 
+void change_device_mode() {
+  switch (current_device_mode) {
+    case 0:  // color mode
+      digitalWrite(LED_DEVICE_MODE_COLOR_PIN, HIGH);
+      digitalWrite(LED_DEVICE_MODE_MUSIC_PIN, LOW);
+      break;
+    case 1:  // music mode
+      digitalWrite(LED_DEVICE_MODE_COLOR_PIN, LOW);
+      digitalWrite(LED_DEVICE_MODE_MUSIC_PIN, HIGH);
+      break;
+  }
+}
 
 void loop() {
 
@@ -236,12 +282,12 @@ void loop() {
         case IR_BTN_NEXT:   // button "Next"        
           if (!device_state)
             break;
-          if (++current_mode >= MODE_COUNT) current_mode = 0; // switch to next mode
+          if (++current_color_mode >= MODE_COUNT) current_color_mode = 0; // switch to next mode
           break;
         case IR_BTN_PREV:   // button "Previous"        
           if (!device_state)
               break;
-          if (--current_mode < 0) current_mode = MODE_COUNT - 1; // switch to previous mode
+          if (--current_color_mode < 0) current_color_mode = MODE_COUNT - 1; // switch to previous mode
           break;
         case IR_BTN_MINUS:  // button "-"
           if (!device_state)
@@ -265,25 +311,25 @@ void loop() {
           if (!device_state)
               break;
               
-          current_mode = 0; // change current mode to 0
+          current_color_mode = 0; // change current mode to 0
           break;
         case IR_BTN_2:  // button "2"
           if (!device_state)
             break;
                 
-          current_mode = 1; // change current mode to 1
+          current_color_mode = 1; // change current mode to 1
           break;
         case IR_BTN_3:  // button "3"
           if (!device_state)
             break;
                 
-          current_mode = 2; // change current mode to 2
+          current_color_mode = 2; // change current mode to 2
           break;
         case IR_BTN_4:  // button "4"
           if (!device_state)
             break;
                 
-          current_mode = 3; // change current mode to 3
+          current_color_mode = 3; // change current mode to 3
           break;
       }
     }
@@ -306,7 +352,7 @@ void loop() {
 
   //    switch (IrReceiver.decodedIRData.decodedRawData) {
   //      case IR_BTN_MINUS: // button "-"
-  //       if (--current_mode < 0) current_mode = MODE_COUNT - 1;
+  //       if (--current_color_mode < 0) current_color_mode = MODE_COUNT - 1;
   //       break;
   //     break;        
   //    }
@@ -314,49 +360,52 @@ void loop() {
   //    IrReceiver.resume(); 
   //  }
 
-
-  // check if button was pressed
-  btn_mode_swt_current_state = debounce(BTN_MODE_SWT_PIN, btn_mode_swt_last_state);
+  // check if change color mode button was pressed
+  btn_color_mode_swt_current_state = debounce(BTN_COLOR_MODE_SWT_PIN, btn_color_mode_swt_last_state);
 
   // if last state was low and curret low change mode. Current state low as pressed because used INPUT_PULLUP pin mode
-  if (btn_mode_swt_last_state == HIGH && btn_mode_swt_current_state == LOW)
+  if (btn_color_mode_swt_last_state == HIGH && btn_color_mode_swt_current_state == LOW)
   {
-    if (++current_mode >= MODE_COUNT) current_mode = 0;
+    if (++current_color_mode >= MODE_COUNT) current_color_mode = 0;
   }
 
-  btn_mode_swt_last_state = btn_mode_swt_current_state;
+  btn_color_mode_swt_last_state = btn_color_mode_swt_current_state;
+
+  // check if change device mode button was pressed
+  btn_device_mode_swt_current_state = debounce(BTN_DEVICE_MODE_SWT_PIN, btn_device_mode_swt_last_state);
+
+  // if last state was low and curret low change mode. Current state low as pressed because used INPUT_PULLUP pin mode
+  if (btn_device_mode_swt_last_state == HIGH && btn_device_mode_swt_current_state == LOW)
+  {
+    if (++current_device_mode >= 2) current_device_mode = 0;
+
+    change_device_mode();
+  }
+
+  btn_device_mode_swt_last_state = btn_device_mode_swt_current_state;
 
   // check current mode and run function for show leds
   if (device_state) {
     if (millis() - global_timer > GLOBAL_LOOP) {
       global_timer = millis();  // reset main timer
 
-      switch (current_mode) {
-        case 0:
-          if (millis() - mode_timer > MODE_SPEED_0) {
-            mode_timer = millis();  // reset mode timer
+      if (millis() - mode_timer > COLOR_MODE_SPEED[current_color_mode]) {
+        mode_timer = millis();  // reset mode timer
 
-            mode_0();
-          }
-          break;
-        case 1:
-          if (millis() - mode_timer > MODE_SPEED_1) {
-            mode_timer = millis();  // reset mode timer
-            mode_1();
-          } 
-          break;
-        case 2:
-          if (millis() - mode_timer > MODE_SPEED_2) {
-            mode_timer = millis();  // reset mode timer
-            mode_2();
-          } 
-          break;
-        case 3:
-          if (millis() - mode_timer > MODE_SPEED_3) {
-            mode_timer = millis();  // reset mode timer
-            mode_3();
-          } 
-          break;
+        switch (current_color_mode) {
+          case 0:
+            mode_color_0();
+            break;
+          case 1:
+            mode_color_1();
+            break;
+          case 2:
+            mode_color_2();
+            break;
+          case 3:
+            mode_color_3();
+            break;
+        }
       }
     }
 
